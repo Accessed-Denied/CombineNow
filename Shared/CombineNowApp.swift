0af0bd4f1a783c60845dc8dab0f9fd005ec8d187
Rohit Saini <sketchlearn7@gmail.com>
2020-12-17 15:15:34 +0530

    Initial Commit

---
+++ Shared/CombineNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineNowApp.swift
+//  Shared
+//
+//  Created by Rohit Saini on 17/12/20.
+//
+
+import SwiftUI
+
+@main
+struct CombineNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
